b004d967f26c9b2a56b66af840903099fad4de46
nishanttaneja <nishant.taneja2701@gmail.com>
2020-08-21 16:49:21 +0530

    add AllClear

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -11,23 +11,25 @@ import UIKit
 class ViewController: UIViewController {
     // IBOutlet
     @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var allClearButton: UIButton!
     
     // Variables
     private var didFinishEnteringNumber: Bool = true
     
     // IBActions
     @IBAction func numberSelected(_ sender: UIButton!) {
+        if allClearButton.currentTitle == "AC" {allClearButton.setTitle("C", for: .normal)}
         if sender.currentTitle == "." {
             if didFinishEnteringNumber {numberLabel.text = "0."; didFinishEnteringNumber=false; return}
             if !numberLabel.text!.contains(".") {numberLabel.text! += "."}
         } else {
             if didFinishEnteringNumber {numberLabel.text = sender.currentTitle; didFinishEnteringNumber=false}
             else {numberLabel.text! += sender.currentTitle!}
-            
         }
     }
     @IBAction func operationSelected(_ sender: UIButton!) {
         didFinishEnteringNumber = true
+        if sender.currentTitle == "C" {allClearButton.setTitle("AC", for: .normal); numberLabel.text = "0"}
     }
 }
 
